f3f5b245b8fbee32252c260ee91500b04469ac47
Jason Washburn <jason.washburn@gmail.com>
2022-12-09 20:44:31 -0600

    calls lowercase from hello

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,14 +1,37 @@
 use anyhow::Result;
 use serde_json::json;
+use serde::{Serialize, Deserialize};
 use spin_sdk::{
     http::{Request, Response},
     http_component,
 };
 
+#[derive(Deserialize, Serialize)]
+struct LowercaseRequest {
+    value: String,
+}
+
+
 /// A simple Spin HTTP component.
 #[http_component]
 fn advent_of_spin_1(req: Request) -> Result<Response> {
     println!("{:?}", req.headers());
+    let name = dbg!(req.headers().get("spin-path-info").unwrap().to_str().unwrap());
+
+    let lower_req = LowercaseRequest {
+        value: name.to_string().strip_prefix("/").unwrap().to_string(),
+    };
+
+    let mut lowercase_resp = spin_sdk::outbound_http::send_request(
+        http::Request::builder()
+            .method("POST")
+            .uri("http://127.0.0.1:3000/lowercase")
+            .body(Some(serde_json::to_string(&lower_req).unwrap().into())).unwrap()
+    );
+//    lowercase_resp.headers_mut()
+//        .insert("spin-component", "rust-outbound-http".try_into()?);
+    println!("{:?}", lowercase_resp);
+
     let resp = json!({
         "message": "Hello, world!"
     });
